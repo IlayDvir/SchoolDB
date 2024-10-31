797e7217f0612d56672e411fb4d8650dca97b434
IlayDvir <idvir222@gmail.com>
2024-10-31 17:01:46 -0400

    b

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+from flask import Flask, request
+from flask import Flask, render_template, request, redirect, url_for
+
+
 import mysql.connector
 
 app = Flask(__name__)
@@ -6,7 +10,7 @@ app = Flask(__name__)
 # Database connection
 db = mysql.connector.connect(
     port = 3306,
-    host = "10.4.65.59",
+    host = "10.4.56.134",
     user="root",
     password="BestPassword",
     database="University"
@@ -23,6 +27,27 @@ def home():
     except Exception as e:
         print(f"Error: {e}")  # Print error to console
         return f"An error occurred: {e}"
+
+@app.route('/insertstudent', methods=['POST'])
+def insertstudent():
+
+
+    id = request.form.get('ID')
+    name = request.form.get('name')
+    age = request.form.get('Age')
+    insert_query = "INSERT INTO my_table (id, name, age) VALUES (%s, %s, %s)"
+    data = (id, name, age)
+
+    # Use the data as needed
+    #+str(name)+", "
+    cursor = db.cursor(dictionary=True)
+    cursor.execute(insert_query, data)
+
+    return redirect(url_for('index'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
